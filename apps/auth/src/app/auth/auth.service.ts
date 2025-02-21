2cea8d226632adf983559aac207f4e1f25924728
Martin Macharia <mosmartin5@gmail.com>
2025-02-21 16:00:39 +0100

    feat(auth): implement login functionality with JWT and context support

--- apps/auth/src/app/auth/auth.service.ts
+++ apps/auth/src/app/auth/auth.service.ts
@@ -1,4 +1,59 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, UnauthorizedException } from '@nestjs/common';
+import { LoginInput } from './dto/login.input';
+import { UsersService } from '../users/users.service';
+import { compare } from 'bcryptjs';
+import { ConfigService } from '@nestjs/config';
+import { TokenPayload } from './token-payload.interface';
+import { JwtService } from '@nestjs/jwt';
+import { Response } from 'express';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly configService: ConfigService,
+    private readonly jwtService: JwtService
+  ) {}
+
+  async login({ email, password }: LoginInput, res: Response) {
+    const user = await this.verifyUser(email, password);
+
+    const expires = new Date();
+    expires.setMilliseconds(
+      expires.getTime() +
+        parseInt(this.configService.getOrThrow('JWT_EXPIRES_MS'))
+    );
+
+    const tokenPayload: TokenPayload = {
+      userId: user.id,
+    };
+
+    const token = this.jwtService.sign(tokenPayload);
+    res.cookie('Authentication', token, {
+      httpOnly: true,
+      secure: this.configService.get('NODE_ENV') === 'production',
+      expires,
+    });
+
+    return user;
+  }
+
+  private async verifyUser(email: string, password: string) {
+    try {
+      const user = await this.usersService.findOne({ email });
+      if (!user) {
+        throw new UnauthorizedException('Invalid credentials');
+      }
+
+      const isPasswordValid = await compare(password, user.password);
+      if (!isPasswordValid) {
+        throw new UnauthorizedException('Invalid credentials');
+      }
+
+      return user;
+    } catch (error) {
+      Logger.error(error);
+      throw new UnauthorizedException('Invalid credentials');
+    }
+  }
+}
--- apps/auth/src/app/users/users.service.ts
+++ apps/auth/src/app/users/users.service.ts
@@ -19,4 +19,10 @@ export class UsersService {
       },
     });
   }
+
+  async findOne(args: Prisma.UserWhereUniqueInput) {
+    return this.prismaService.user.findUniqueOrThrow({
+      where: args,
+    });
+  }
 }
