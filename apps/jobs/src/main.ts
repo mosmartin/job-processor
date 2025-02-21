64f5ac52801451ca5cd7de62dd8bb59eaa12b47e
Martin Macharia <mosmartin5@gmail.com>
2025-02-21 23:12:17 +0100

    refactor(jobs): remove unused AppController and AppService, update AppModule for global config

--- apps/jobs/src/main.ts
+++ apps/jobs/src/main.ts
@@ -1,18 +1,20 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import { Logger } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
+import { ConfigService } from '@nestjs/config';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
+
+  const port = app.get(ConfigService).getOrThrow('PORT');
   await app.listen(port);
+
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
   );
