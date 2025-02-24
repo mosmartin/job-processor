eb48624cc1929d0917a96c51fb9b6f8b7f26f5fe
Martin Macharia <mosmartin5@gmail.com>
2025-02-24 14:50:10 +0100

    feat(auth): integrate gRPC transport and update proto configuration

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,8 +1,11 @@
+import { AUTH_PACKAGE_NAME } from 'types/proto/auth';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { ConfigService } from '@nestjs/config';
 import cookieParser from 'cookie-parser';
+import { GrpcOptions, Transport } from '@nestjs/microservices';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +16,16 @@ async function bootstrap() {
   app.setGlobalPrefix(globalPrefix);
 
   const port = app.get(ConfigService).getOrThrow('PORT');
+
+  app.connectMicroservice<GrpcOptions>({
+    transport: Transport.GRPC,
+    options: {
+      package: AUTH_PACKAGE_NAME,
+      protoPath: join(__dirname, 'proto/auth.proto'),
+    },
+  });
+
+  await app.startAllMicroservices();
   await app.listen(port);
 
   Logger.log(
